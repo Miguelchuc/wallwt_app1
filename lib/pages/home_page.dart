0930cbf828ecd02f8685c6ad3b345547a2fed3f1
Miguel chuc <limonmike14@gmail.com>
2025-05-20 18:21:13 -0600

    Buttons

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import 'package:wallwt_app1/util/my_button.dart';
 import 'package:wallwt_app1/util/my_card.dart';
 
 class HomePage extends StatefulWidget {
@@ -97,10 +98,95 @@ class _HomePageState extends State<HomePage> {
               effect: ExpandingDotsEffect(
                 activeDotColor: Colors.grey.shade800,
               ),
-            )
+            ),
+
+            SizedBox(height: 25),
+
             // los 3 botones enviar pago y factura
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  //boton de enviar
+
+                  MyButton(
+                    iconImagePath: 'lib/icons/enviar.png',
+                    buttonText: 'Enviar',
+                  ),
+
+                  //boton de pago
+
+                  MyButton(
+                    iconImagePath: 'lib/icons/tarjeta.png',
+                    buttonText: 'Enviar',
+                  ),
+
+                  //boton de facturas
+
+                  MyButton(
+                    iconImagePath: 'lib/icons/factura.png',
+                    buttonText: 'facturar',
+                  ),
+                ],
+              ),
+            ),
+
+            SizedBox(height: 25),
 
             // columna con las estadisticas y las trasacciones
+            Padding(
+              padding: const EdgeInsets.all(2.0),
+              child: Column(
+                children: [
+                  //estadisticas
+                  Row(
+                    children: [
+                      //icono de las estadisticas s
+                      Container(
+                        height: 80,
+                        padding: EdgeInsets.all(12),
+                        decoration: BoxDecoration(
+                            color: Colors.grey[100],
+                            borderRadius: BorderRadius.circular(12)),
+                        child: Image.asset('lib/icons/estadisticas.png'),
+                      ),
+
+                      SizedBox(
+                        width: 20,
+                      ),
+
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'estadisticas',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 12,
+                          ),
+                          Text(
+                            'pagos y compras',
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.grey[600],
+                            ),
+                          )
+                        ],
+                      ),
+
+                      Icon(Icons.arrow_forward_ios),
+                    ],
+                  )
+
+                  // transacciones
+                ],
+              ),
+            )
           ],
         ),
       ),
---
+++ lib/util/my_button.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class MyButton extends StatelessWidget {
+  final String iconImagePath;
+  final String buttonText;
+
+  const MyButton({
+    super.key,
+    required this.iconImagePath,
+    required this.buttonText,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        //icono
+        Container(
+          height: 100,
+          padding: EdgeInsets.all(20),
+          decoration: BoxDecoration(
+              color: Colors.grey[100],
+              borderRadius: BorderRadius.circular(20),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.shade500,
+                  blurRadius: 40,
+                  spreadRadius: 10,
+                )
+              ]),
+          child: Center(
+            child: Image.asset(iconImagePath),
+          ),
+        ),
+        SizedBox(
+          height: 12,
+        ),
+        //texto
+        Text(
+          buttonText,
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            color: Colors.grey[700],
+          ),
+        )
+      ],
+    );
+  }
+}
